e5732c07ca80f86a0aa317b79c2ab348f945ca82
Tim Griesser <tgriesser10@gmail.com>
2013-06-19 14:42:21 -0400

    adding a plugin for node callback interface exec method, fixes #20

---
+++ plugins/exec.js
@@ -0,0 +1,39 @@
+var Bookshelf = require('../bookshelf');
+var _         = require('underscore');
+
+// Used to optionally add `exec` support for those who prefer node-style callbacks.
+Bookshelf.wrapExec = function(target, method) {
+  var targetMethod = target[method];
+  target[method] = function() {
+    var args = arguments;
+    var ctx = this;
+    var result;
+    return {
+      then: function(onFulfilled, onRejected) {
+        result || (result = targetMethod.apply(ctx, args));
+        return result.then(onFulfilled, onRejected);
+      },
+      exec: function(callback) {
+        result || (result = targetMethod.apply(ctx, args));
+        return targetMethod.apply(ctx, args).then(function(resp) {
+          callback(null, resp);
+        }, function(err) {
+          callback(err, null);
+        }).then(null, function(err) {
+          setTimeout(function() { throw err; }, 0);
+        });
+      }
+    };
+  };
+};
+
+_.each(['load', 'fetch', 'save', 'destroy'], function(method) {
+  Bookshelf.wrapExec(Bookshelf.Model.prototype, method);
+});
+
+_.each(['load', 'fetch'], function(method) {
+  Bookshelf.wrapExec(Bookshelf.Collection.prototype, method);
+});
+
+// Export the `Bookshelf` object.
+module.exports = Bookshelf;
