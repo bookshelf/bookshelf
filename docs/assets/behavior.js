ef48c8c056284d3c7887e671bacdeb4a3d9a1243
Tim Griesser <tgriesser10@gmail.com>
2013-10-10 18:36:32 -0400

    docs prep for 0.5.6

--- docs/assets/behavior.js
+++ docs/assets/behavior.js
@@ -25,8 +25,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
-            "title": "Bookshelf.js 0.5.5",
-            "slug": "bookshelfjs-055"
+            "title": "Bookshelf.js 0.5.6",
+            "slug": "bookshelfjs-056"
           },
           "depth": 2
         }
