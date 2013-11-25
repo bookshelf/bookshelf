1d8854424e475caa5bc935de077b10c763348bb5
Tim Griesser <tgriesser10@gmail.com>
2013-11-25 12:50:42 -0500

    bumping knex dep, updating Relations test output

--- test/integration/output/Relations.js
+++ test/integration/output/Relations.js
@@ -367,11 +367,11 @@ module.exports = {
       result: {
         id: 3,
         name: 'backbonejs.org',
-        blogs: [],
         meta: {
         
         },
-        authors: []
+        authors: [],
+        blogs: []
       }
     },
     sqlite3: {
@@ -1038,35 +1038,6 @@ module.exports = {
       result: {
         id: 1,
         name: 'knexjs.org',
-        blogs: [{
-          id: 1,
-          site_id: 1,
-          name: 'Main Site Blog',
-          posts: [{
-            id: 1,
-            owner_id: 1,
-            blog_id: 1,
-            name: 'This is a new Title!',
-            content: 'Lorem ipsum Labore eu sed sed Excepteur enim laboris deserunt adipisicing dolore culpa aliqua cupidatat proident ea et commodo labore est adipisicing ex amet exercitation est.'
-          },{
-            id: 3,
-            owner_id: 2,
-            blog_id: 1,
-            name: 'This is a new Title 3!',
-            content: 'Lorem ipsum Reprehenderit esse esse consectetur aliquip magna.'
-          }]
-        },{
-          id: 2,
-          site_id: 1,
-          name: 'Alternate Site Blog',
-          posts: [{
-            id: 2,
-            owner_id: 2,
-            blog_id: 2,
-            name: 'This is a new Title 2!',
-            content: 'Lorem ipsum Veniam ex amet occaecat dolore in pariatur minim est exercitation deserunt Excepteur enim officia occaecat in exercitation aute et ad esse ex in in dolore amet consequat quis sed mollit et id incididunt sint dolore velit officia dolor dolore laboris dolor Duis ea ex quis deserunt anim nisi qui culpa laboris nostrud Duis anim deserunt esse laboris nulla qui in dolor voluptate aute reprehenderit amet ut et non voluptate elit irure mollit dolor consectetur nisi adipisicing commodo et mollit dolore incididunt cupidatat nulla ut irure deserunt non officia laboris fugiat ut pariatur ut non aliqua eiusmod dolor et nostrud minim elit occaecat commodo consectetur cillum elit laboris mollit dolore amet id qui eiusmod nulla elit eiusmod est ad aliqua aute enim ut aliquip ex in Ut nisi sint exercitation est mollit veniam cupidatat adipisicing occaecat dolor irure in aute aliqua ullamco.'
-          }]
-        }],
         authors: [{
           id: 1,
           site_id: 1,
@@ -1104,6 +1075,35 @@ module.exports = {
             blog_id: 1,
             name: 'This is a new Title 3!',
             content: 'Lorem ipsum Reprehenderit esse esse consectetur aliquip magna.'
+          }]
+        }],
+        blogs: [{
+          id: 1,
+          site_id: 1,
+          name: 'Main Site Blog',
+          posts: [{
+            id: 1,
+            owner_id: 1,
+            blog_id: 1,
+            name: 'This is a new Title!',
+            content: 'Lorem ipsum Labore eu sed sed Excepteur enim laboris deserunt adipisicing dolore culpa aliqua cupidatat proident ea et commodo labore est adipisicing ex amet exercitation est.'
+          },{
+            id: 3,
+            owner_id: 2,
+            blog_id: 1,
+            name: 'This is a new Title 3!',
+            content: 'Lorem ipsum Reprehenderit esse esse consectetur aliquip magna.'
+          }]
+        },{
+          id: 2,
+          site_id: 1,
+          name: 'Alternate Site Blog',
+          posts: [{
+            id: 2,
+            owner_id: 2,
+            blog_id: 2,
+            name: 'This is a new Title 2!',
+            content: 'Lorem ipsum Veniam ex amet occaecat dolore in pariatur minim est exercitation deserunt Excepteur enim officia occaecat in exercitation aute et ad esse ex in in dolore amet consequat quis sed mollit et id incididunt sint dolore velit officia dolor dolore laboris dolor Duis ea ex quis deserunt anim nisi qui culpa laboris nostrud Duis anim deserunt esse laboris nulla qui in dolor voluptate aute reprehenderit amet ut et non voluptate elit irure mollit dolor consectetur nisi adipisicing commodo et mollit dolore incididunt cupidatat nulla ut irure deserunt non officia laboris fugiat ut pariatur ut non aliqua eiusmod dolor et nostrud minim elit occaecat commodo consectetur cillum elit laboris mollit dolore amet id qui eiusmod nulla elit eiusmod est ad aliqua aute enim ut aliquip ex in Ut nisi sint exercitation est mollit veniam cupidatat adipisicing occaecat dolor irure in aute aliqua ullamco.'
           }]
         }]
       }
@@ -2753,7 +2753,7 @@ module.exports = {
         instance_id: 3,
         route: null,
         instance: {
-          id: 3,
+          id: '3',
           name: 'search engine'
         }
       }
@@ -2796,7 +2796,7 @@ module.exports = {
         instance_id: 3,
         route: null,
         instance: {
-          id: 3,
+          id: '3',
           name: 'search engine'
         }
       },{
@@ -2804,7 +2804,7 @@ module.exports = {
         instance_id: 10,
         route: null,
         instance: {
-          id: 10,
+          id: '10',
           name: 'computers'
         }
       }]
