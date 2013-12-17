e2d19bdefaa1ccd1c98e6978500baf6af73aaad9
Tim Griesser <tgriesser10@gmail.com>
2013-12-17 16:14:22 -0500

    cleaner impl of nodeMethod

--- base/promise.js
+++ base/promise.js
@@ -1,21 +1,5 @@
 
 var Promise = require('bluebird/js/main/promise')();
-var util    = require('bluebird/js/main/util');
-
-var errorObj  = util.errorObj;
-var tryCatch1 = util.tryCatch1;
-var tryCatch2 = util.tryCatch2;
-var tryCatchApply = util.tryCatchApply;
-var tryCatch3 = function tryCatch3(fn, receiver, arg, arg2, arg3) {
-  try {
-    return fn.call(receiver, arg, arg2, arg3);
-  } catch (e) {
-    errorObj.e = e;
-    return errorObj;
-  }
-};
-
-var tryCatchApply = util.tryCatchApply;
 
 Promise.prototype.yield = function(value) {
   return this.then(function() {
@@ -34,39 +18,17 @@ Promise.resolve = Promise.fulfilled;
 Promise.reject  = Promise.rejected;
 
 Promise.nodeMethod = function Promise$_NodeMethod(fn) {
-  if (typeof fn !== "function") throw new TypeError("fn must be a function");
   return function Promise$_nodeMethod() {
-    var value, nodeFn;
-    if (arguments.length > 0) {
-      var end = arguments.length - 1;
-      nodeFn  = arguments[end];
-      if (typeof nodeFn === "function") {
-        var $_len = arguments.length;
-        var args = new Array(end - 0);
-        for (var $_i = 0; $_i < end; ++$_i) {
-          args[$_i - 0] = arguments[$_i];
-        }
-        value = tryCatchApply(fn, args, this);
-      } else {
-        switch(arguments.length) {
-        case 1: value = tryCatch1(fn, this, arguments[0]); break;
-        case 2: value = tryCatch2(fn, this, arguments[0], arguments[1]); break;
-        case 3: value = tryCatch3(fn, this, arguments[0], arguments[1], arguments[2]); break;
-        default:
-          var $_len2 = arguments.length;
-          var args2 = new Array($_len2);
-          for (var $_i2 = 0; $_i2 < $_len2; ++$_i2) {
-            args2[$_i2] = arguments[$_i2];
-          }
-          value = tryCatchApply(fn, args2, this); break;
-        }
-      }
-    } else {
-      value = tryCatch1(fn, this, void 0);
+    var nodeFn, $_len = arguments.length;
+    var args = new Array($_len);
+    for (var $_i = 0; $_i < $_len; ++$_i) {
+      args[$_i] = arguments[$_i];
     }
-    var ret = new Promise(function(){});
-        ret._resolveFromSyncValue(value, Promise$_nodeMethod);
-    return ret.nodeify(nodeFn);
+    if ($_len > 0) {
+      nodeFn = args[$_len - 1];
+      if (typeof nodeFn === "function") args.pop();
+    }
+    return Promise.method(fn).apply(this, args).nodeify(nodeFn);
   };
 };
 
