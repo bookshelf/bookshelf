d06c3cdee343af023141eba62df51d4290ea57fe
Tim Griesser <tgriesser10@gmail.com>
2014-01-13 23:24:41 -0500

    Adding null defaults for visible and defaults
    For clarity, as mentioned by @nathggns in #186

--- plugins/visibility.js
+++ plugins/visibility.js
@@ -1,11 +1,23 @@
+// Visibility plugin - useful for hiding/showing particular
+// attributes on `toJSON`.
+// -----
 module.exports = function(Bookshelf) {
   "use strict";
+
   var _      = require('lodash');
   var proto  = Bookshelf.Model.prototype;
   var toJSON = proto.toJSON;
 
   var Model = Bookshelf.Model.extend({
 
+    // Replace with an array of properties to blacklist on `toJSON`.
+    hidden: null,
+
+    // Replace with an array of properties to whitelist on `toJSON`.
+    visible: null,
+
+    // If `visible` or `hidden` are specified in the `options` hash,
+    // they're assumed to override whatever is on the model's prototype.
     constructor: function() {
       proto.constructor.apply(this, arguments);
       var options = arguments[1] || {};
@@ -17,6 +29,8 @@ module.exports = function(Bookshelf) {
       }
     },
 
+    // Checks the `visible` and then `hidden` properties to see if there are
+    // any keys we don't want to show when the object is json-ified.
     toJSON: function() {
       var json = toJSON.apply(this, arguments);
       if (this.visible) {
