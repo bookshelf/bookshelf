9a9a4108f1b99f9498b208435bfdcc0699ed060d
Johannes Lumpe <johannes@johanneslumpe.de>
2014-06-14 00:31:34 +0300

    And again left the debug calls in...

--- plugins/touch.js
+++ plugins/touch.js
@@ -15,7 +15,6 @@ var updatedBelongsToMany = function (relation, key, update, collection) {
   // where using `whereIn`
   return relation.query()
   .update(update)
-  .debug()
   .whereIn(key, _.pluck(collection.models, id));
 };
 
@@ -59,7 +58,6 @@ module.exports = function(Bookshelf) {
         } else {
           query = relation.query();
           relatedData.whereClauses(query);
-          query.debug();
           query.update(update);
         }
         toTouch.push(
