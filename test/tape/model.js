9881797e7c7794d9a375aa94897ad6a0e742a330
Tim Griesser <tgriesser10@gmail.com>
2015-05-08 16:53:48 -0400

    Patch for issue #730

---
+++ test/tape/model.js
@@ -0,0 +1,9 @@
+var test  = require('tape')
+var Knex  = require('knex')
+var Model = require('../../lib/model')
+
+test('#730, model.query().fetch() does not limit results', function() {
+
+  // TODO
+
+})
