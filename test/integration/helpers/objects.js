4c7f677a88078373950f7d2d2f2b1ca1970788e7
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-06-07 18:29:32 +1000

    Update integration test models.
    `Post` and `Comment` models had default fields that are not present in schema. `Post` additionally used the `hasTimestamps` flag without having timestamp columns.

--- test/integration/helpers/objects.js
+++ test/integration/helpers/objects.js
@@ -142,12 +142,10 @@ module.exports = function(Bookshelf) {
   var Post = Bookshelf.Model.extend({
     tableName: 'posts',
     defaults: {
-      author: '',
-      title: '',
-      body: '',
-      published: false
+      name: '',
+      content: ''
     },
-    hasTimestamps: true,
+    hasTimestamps: false,
     blog: function() {
       return this.belongsTo(Blog);
     },
@@ -175,7 +173,7 @@ module.exports = function(Bookshelf) {
     tableName: 'comments',
     defaults: {
       email: '',
-      post: ''
+      comment: ''
     },
     posts: function() {
       return this.belongsTo(Post);
