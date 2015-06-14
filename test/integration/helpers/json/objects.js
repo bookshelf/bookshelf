fe55ec63dc5e609a350fc625bfdf18c477c7304a
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-06-15 01:26:55 +1000

    Only use idAttribute to fetch a model, always exclude object attributes.
    Previous fetch behaviour was causing unexpected behaviour. See issues below:
    
    Fixes tgriesser/bookshelf#550, tgriesser/bookshelf#778

---
+++ test/integration/helpers/json/objects.js
@@ -0,0 +1,27 @@
+// Models used for PostgreSQL jsonb type.
+module.exports = function(Bookshelf) {
+
+  var _ = require('lodash');
+
+  var Unit = Bookshelf.Model.extend({
+    tableName: 'units',
+    commands: function() {
+      return this.hasMany(Command);
+    }
+  });
+
+  var Command = Bookshelf.Model.extend({
+    tableName: 'commands',
+    unit: function() {
+      return this.belongsTo(Unit);
+    }
+  });
+
+  return {
+    Models: {
+      Unit: Unit,
+      Command: Command
+    }
+  };
+
+};
