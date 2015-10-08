f49cf45cc02031fcf80c3106c7c64c66cda8d4b5
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-10-08 19:37:35 +1100

    Ignore compiled source and generated documentation.
    Master branch no longer keeps generated content, as it is now added to the `.gitignore`. Instead these files will be force added to release commits only.

---
+++ scripts/jsdoc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+static_assets=./.jsdoc-temp
+
+# Clean existing docs
+rm -rf ./docs index.html
+
+# Create a temporary folder for static assets.
+rm -rf $static_assets
+mkdir $static_assets
+
+# Generate static assets. (assumes uglifyjs is installed globally...)
+uglifyjs node_modules/knex/build/knex.js > $static_assets/knex.min.js
+
+# Then create the docs.
+$(npm bin)/jsdoc --configure ./scripts/jsdoc.config.json
+
+# Now remove temporary folder to clean up.
+rm -rf $static_assets
