492303f2908cbdb9804cb2909ab880a1dbeb5a2e
Basil Siddiqui <basil.siddiqui@gmail.com>
2015-10-13 00:18:33 -0700

    Fix password validation example in `Model.extend` docs
    Fixes #854

--- src/base/model.js
+++ src/base/model.js
@@ -582,7 +582,10 @@ _.each(modelMethods, function(method) {
  *       login: Promise.method(function(email, password) {
  *         if (!email || !password) throw new Error('Email and password are both required');
  *         return new this({email: email.toLowerCase().trim()}).fetch({require: true}).tap(function(customer) {
- *           return bcrypt.compareAsync(customer.get('password'), password);
+ *           return bcrypt.compareAsync(customer.get('password'), password)
+ *            .then(function(res) {
+ *              if (!res) throw new Error('Invalid password');
+ *            });
  *         });
  *       })
  *     
