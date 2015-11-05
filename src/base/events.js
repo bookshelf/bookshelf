10ca29cc7cc41343f1c6caad6b97a143a4e0a614
Ricardo Graça <ricardo@devius.net>
2015-11-05 16:44:39 +0000

    Remove links to Backbone from Events module

--- src/base/events.js
+++ src/base/events.js
@@ -11,19 +11,24 @@ const flatMap = flow(map, flatten);
 
 /**
  * @class Events
- * @class
- *
- * Bookshelf includes events based on those provided by
- * [Backbone](http://backbonejs.org/).
- *
+ * @description
+ * Base Event class inherited by {@link Model} and {@link Collection}. It's not
+ * meant to be used directly, and is only displayed here for completeness.
  */
 export default class Events extends EventEmitter {
 
   /**
    * @method Events#on
    * @description
-   * Register an event listener.
-   * @see {@link http://backbonejs.org/#Events-on Backbone.js `Events#on`}
+   * Register an event listener. The callback will be invoked whenever the event
+   * is fired. The event string may also be a space-delimited list of several
+   * event names.
+   *
+   * @param {string} eventName
+   *   The name of the event or space separated list of events to register a
+   *   callback for.
+   * @param {function} callback
+   *   That callback to invoke whenever the event is fired.
    */
   on(nameOrNames, handler, ...args) {
     for (const name of words(nameOrNames)) {
@@ -35,8 +40,12 @@ export default class Events extends EventEmitter {
   /**
    * @method Events#off
    * @description
-   * Deregister an event listener.
-   * @see {@link http://backbonejs.org/#Events-off Backbone.js `Events#off`}
+   * Remove a previously-bound callback event listener from an object. If no
+   * event name is specified, callbacks for all events will be removed.
+   *
+   * @param {string} eventName
+   *   The name of the event or space separated list of events to stop listening
+   *   to.
    */
   off(nameOrNames, listener) {
     if (nameOrNames == null) {
@@ -56,8 +65,15 @@ export default class Events extends EventEmitter {
   /**
    * @method Events#trigger
    * @description
-   * Deregister an event listener.
-   * @see {@link http://backbonejs.org/#Events-trigger Backbone.js `Events#trigger`}
+   * Trigger callbacks for the given event, or space-delimited list of events.
+   * Subsequent arguments to `trigger` will be passed along to the event
+   * callback.
+   *
+   * @param {string} eventName
+   *   The name of the event to trigger. Also accepts a space separated list of
+   *   event names.
+   * @param {mixed} [arguments]
+   *   Extra arguments to pass to the event listener callback function.
    */
   trigger(nameOrNames, ...args) {
     for (const name of nameOrNames) {
@@ -96,8 +112,17 @@ export default class Events extends EventEmitter {
   /**
    * @method Events#once
    * @description
-   * Register a one-off event handler.
-   * @see {@link http://backbonejs.org/#Events-once Backbone.js `Events#once`}
+   * Just like {@link Events#on}, but causes the bound callback to fire only
+   * once before being removed. Handy for saying "the next time that X happens,
+   * do this". When multiple events are passed in using the space separated
+   * syntax, the event will fire once for every event you passed in, not once
+   * for a combination of all events.
+   *
+   * @param {string} eventName
+   *   The name of the event or space separated list of events to register a
+   *   callback for.
+   * @param {function} callback
+   *   That callback to invoke only once when the event is fired.
    */
   once(name, callback, context) {
     const once = _.once(() => {
