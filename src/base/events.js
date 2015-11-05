153daf912a6f3ff9968cd958bd432e2e51f97615
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-11-05 19:49:34 +1100

    Use `class` notation for `Events`.

--- src/base/events.js
+++ src/base/events.js
@@ -2,7 +2,6 @@
 // ---------------
 
 import Promise from './promise';
-import inherits from 'inherits';
 import events from 'events'
 import _, { each, flatten, flow, map, words } from 'lodash';
 
@@ -18,99 +17,96 @@ const flatMap = flow(map, flatten);
  * [Backbone](http://backbonejs.org/).
  *
  */
-function Events() {
-  EventEmitter.apply(this, arguments);
+export default class Events extends EventEmitter {
+
+  /**
+   * @method Events#on
+   * @description
+   * Register an event listener.
+   * @see {@link http://backbonejs.org/#Events-on Backbone.js `Events#on`}
+   */
+  on(nameOrNames, handler, ...args) {
+    for (const name of words(nameOrNames)) {
+      EventEmitter.prototype.on.apply(this, [name, handler, ...args]);
+    }
+    return this;
+  }
+
+  /**
+   * @method Events#off
+   * @description
+   * Deregister an event listener.
+   * @see {@link http://backbonejs.org/#Events-off Backbone.js `Events#off`}
+   */
+  off(nameOrNames, listener) {
+    if (nameOrNames == null) {
+      return listener == null
+        ? this.removeAllListeners()
+        : this.removeAllListeners(listener);
+    }
+
+    each(words(nameOrNames), listener == null
+      ? name => this.removeAllListeners(name)
+      : name => this.removeAllListeners(name, listener)
+    );
+
+    return this;
+  }
+
+  /**
+   * @method Events#trigger
+   * @description
+   * Deregister an event listener.
+   * @see {@link http://backbonejs.org/#Events-trigger Backbone.js `Events#trigger`}
+   */
+  trigger(nameOrNames, ...args) {
+    for (const name of nameOrNames) {
+      EventEmitter.prototype.emit.apply(this, [name, ...args]);
+    }
+    return this;
+  }
+
+  /**
+   * @method Events#triggerThen
+   * @description
+   * A promise version of {@link Events#trigger}, returning a promise which
+   * resolves with all return values from triggered event handlers. If any of the
+   * event handlers throw an `Error` or return a rejected promise, the promise
+   * will be rejected. Used internally on the {@link Model#creating "creating"},
+   * {@link Model#updating "updating"}, {@link Model#saving "saving"}, and {@link
+   * Model@destroying "destroying"} events, and can be helpful when needing async
+   * event handlers (for validations, etc).
+   *
+   * @param {string} name
+   *   The event name, or a whitespace-separated list of event names, to be
+   *   triggered.
+   * @param {...mixed} args
+   *   Arguments to be passed to any registered event handlers.
+   * @returns Promise<mixed[]>
+   *   A promise resolving the the resolved return values of any triggered handlers.
+   */
+  triggerThen(nameOrNames, ...args) {
+    const names = words(nameOrNames);
+    const listeners = flatMap(names, this.listeners, this);
+    return Promise.map(listeners, listener =>
+      listener.apply(this, args)
+    );
+  }
+
+  /**
+   * @method Events#once
+   * @description
+   * Register a one-off event handler.
+   * @see {@link http://backbonejs.org/#Events-once Backbone.js `Events#once`}
+   */
+  once(name, callback, context) {
+    const once = _.once(() => {
+      this.off(name, once);
+      return callback.apply(this, arguments);
+    });
+    once._callback = callback;
+    return this.on(name, once, context);
+  }
 }
-inherits(Events, EventEmitter);
-
-/**
- * @method Events#on
- * @description
- * Register an event listener.
- * @see {@link http://backbonejs.org/#Events-on Backbone.js `Events#on`}
- */
-Events.prototype.on = function(nameOrNames, handler, ...args) {
-  for (const name of words(nameOrNames)) {
-    EventEmitter.prototype.on.apply(this, [name, handler, ...args]);
-  }
-  return this;
-};
-
-/**
- * @method Events#off
- * @description
- * Deregister an event listener.
- * @see {@link http://backbonejs.org/#Events-off Backbone.js `Events#off`}
- */
-Events.prototype.off = function(nameOrNames, listener) {
-  if (nameOrNames == null) {
-    return listener == null
-      ? this.removeAllListeners()
-      : this.removeAllListeners(listener);
-  }
-
-  each(words(nameOrNames), listener == null
-    ? name => this.removeAllListeners(name)
-    : name => this.removeAllListeners(name, listener)
-  );
-
-  return this;
-};
-
-/**
- * @method Events#trigger
- * @description
- * Deregister an event listener.
- * @see {@link http://backbonejs.org/#Events-trigger Backbone.js `Events#trigger`}
- */
-Events.prototype.trigger = function(nameOrNames, ...args) {
-  for (const name of nameOrNames) {
-    EventEmitter.prototype.emit.apply(this, [name, ...args]);
-  }
-  return this;
-};
-
-/**
- * @method Events#triggerThen
- * @description
- * A promise version of {@link Events#trigger}, returning a promise which
- * resolves with all return values from triggered event handlers. If any of the
- * event handlers throw an `Error` or return a rejected promise, the promise
- * will be rejected. Used internally on the {@link Model#creating "creating"},
- * {@link Model#updating "updating"}, {@link Model#saving "saving"}, and {@link
- * Model@destroying "destroying"} events, and can be helpful when needing async
- * event handlers (for validations, etc).
- *
- * @param {string} name
- *   The event name, or a whitespace-separated list of event names, to be
- *   triggered.
- * @param {...mixed} args
- *   Arguments to be passed to any registered event handlers.
- * @returns Promise<mixed[]>
- *   A promise resolving the the resolved return values of any triggered handlers.
- */
-Events.prototype.triggerThen = function(nameOrNames, ...args) {
-  const names = words(nameOrNames);
-  const listeners = flatMap(names, this.listeners, this);
-  return Promise.map(listeners, listener =>
-    listener.apply(this, args)
-  );
-};
-Events.prototype.emitThen = Events.prototype.triggerThen;
-
-/**
- * @method Events#once
- * @description
- * Register a one-off event handler.
- * @see {@link http://backbonejs.org/#Events-once Backbone.js `Events#once`}
- */
-Events.prototype.once = function(name, callback, context) {
-  const once = _.once(() => {
-    this.off(name, once);
-    return callback.apply(this, arguments);
-  });
-  once._callback = callback;
-  return this.on(name, once, context);
-};
 
 export default Events;
