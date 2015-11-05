7e9d753b9799b0c72cacc76099a05a88fcb4fd41
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-11-05 20:37:59 +1100

    Define `EagerRelation` with `class` syntax.

--- src/eager.js
+++ src/eager.js
@@ -1,76 +1,76 @@
 // EagerRelation
 // ---------------
-import _ from 'lodash'
-import inherits from 'inherits';
+import _, { flatten, groupBy, map, mapValues, uniq } from 'lodash';
 
 import Helpers from './helpers';
 import Promise from './base/promise';
 import EagerBase from './base/eager';
 
+const getAttributeUnique = (models, attribute) =>
+  uniq(map(models, m => m.get(attribute)));
+
 // An `EagerRelation` object temporarily stores the models from an eager load,
-// and handles matching eager loaded objects with their parent(s). The `tempModel`
-// is only used to retrieve the value of the relation method, to know the constrains
-// for the eager query.
-function EagerRelation() {
-  EagerBase.apply(this, arguments);
-}
-inherits(EagerRelation, EagerBase);
+// and handles matching eager loaded objects with their parent(s). The
+// `tempModel` is only used to retrieve the value of the relation method, to
+// know the constraints for the eager query.
+export default class EagerRelation extends EagerBase {
 
-_.extend(EagerRelation.prototype, {
-
-  // Handles an eager loaded fetch, passing the name of the item we're fetching for,
-  // and any options needed for the current fetch.
-  eagerFetch: Promise.method(function(relationName, handled, options) {
+  // Handles an eager loaded fetch, passing the name of the item we're fetching
+  // for, and any options needed for the current fetch.
+  eagerFetch(relationName, handled, options) {
     const relatedData = handled.relatedData;
 
     // skip eager loading for rows where the foreign key isn't set
     if (relatedData.parentFk === null) return;
 
-    if (relatedData.type === 'morphTo') return this.morphToFetch(relationName, relatedData, options);
+    if (relatedData.type === 'morphTo') {
+      return this.morphToFetch(relationName, relatedData, options);
+    }
 
-    return handled
-      .sync(_.extend(options, {parentResponse: this.parentResponse}))
+    return handled.sync({ ...options, parentResponse: this.parentResponse })
       .select()
-      .bind(this)
-      .tap(function(response) {
-        return this._eagerLoadHelper(response, relationName, handled, _.omit(options, 'parentResponse'));
-      });
-  }),
+      .tap((response) => this._eagerLoadHelper(
+        response, relationName, handled, _.omit(options, 'parentResponse')
+      ));
+  }
 
-  // Special handler for the eager loaded morph-to relations, this handles
-  // the fact that there are several potential models that we need to be fetching against.
-  // pairing them up onto a single response for the eager loading.
-  morphToFetch: Promise.method(function(relationName, relatedData, options) {
-    const groups = _.groupBy(this.parent, function(m) {
-      const typeKeyName = relatedData.columnNames && relatedData.columnNames[0] ? relatedData.columnNames[0] : relatedData.morphName + '_type';
-      return m.get(typeKeyName);
-    });
-    const pending = _.reduce(groups, (memo, val, group) => {
-      const Target = Helpers.morphCandidate(relatedData.candidates, group);
-      const target = new Target();
-      const idKeyName = relatedData.columnNames && relatedData.columnNames[1] ? relatedData.columnNames[1] : relatedData.morphName + '_id';
-      memo.push(target
-        .query('whereIn',
-          _.result(target, 'idAttribute'),
-          _.uniq(_.invoke(groups[group], 'get', idKeyName))
-        )
+  // Special handler for the eager loaded morph-to relations, this handles the
+  // fact that there are several potential models that we need to be fetching
+  // against.  pairing them up onto a single response for the eager loading.
+  morphToFetch(relationName, relatedData, options) {
+    const columnNames = relatedData.columnNames || [];
+    const { morphName } = relatedData;
+
+    const [
+      typeColumn = `${morphName}_type`, idColumn = `${morphName}_id`
+    ] = columnNames;
+
+    const parentsByType = groupBy(this.parent, model => model.get(typeColumn));
+    const TargetByType = mapValues(parentsByType, (parents, type) =>
+      Helpers.morphCandidate(relatedData.candidates, type)
+    );
+
+    return Promise.all(map(parentsByType, (parents, type) => {
+      const Target = TargetByType[type];
+      const idAttribute = _.result(Target.prototype, 'idAttribute');
+      const ids = getAttributeUnique(parents, idColumn);
+
+      return Target.query('whereIn', idAttribute, ids)
         .sync(options)
         .select()
-        .bind(this)
-        .tap(function(response) {
-          return this._eagerLoadHelper(response, relationName, {
-            relatedData: relatedData.instance('morphTo', Target, {morphName: relatedData.morphName, columnNames: relatedData.columnNames})
-          }, options);
-        }));
-        return memo;
-    }, []);
-    return Promise.all(pending).then(function(resps) {
-      return _.flatten(resps);
-    });
-  }),
+        .tap(response => {
+          const clone = relatedData.instance(
+            'morphTo', Target, {morphName, columnNames}
+          );
+          return this._eagerLoadHelper(
+            response, relationName, { relatedData: clone }, options
+          );
+        });
+    })).then(flatten);
+  }
 
   // Handles the eager load for both the `morphTo` and regular cases.
-  _eagerLoadHelper: function(response, relationName, handled, options) {
+  _eagerLoadHelper(response, relationName, handled, options) {
     const relatedModels = this.pushModels(relationName, handled, response);
     const relatedData   = handled.relatedData;
 
@@ -87,11 +87,11 @@ _.extend(EagerRelation.prototype, {
       }
       return new EagerRelation(relatedModels, response, relatedModel).fetch(options).return(response);
     }
-  },
+  }
 
   // Filters the `withRelated` on a `morphTo` relation, to ensure that only valid
   // relations are attempted for loading.
-  _filterRelated: function(relatedModel, options) {
+  _filterRelated(relatedModel, options) {
 
     // By this point, all withRelated should be turned into a hash, so it should
     // be fairly simple to process by splitting on the dots.
@@ -103,7 +103,4 @@ _.extend(EagerRelation.prototype, {
       return memo;
     }, []);
   }
-
-});
-
-module.exports = EagerRelation;
+}
