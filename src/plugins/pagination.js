951a72b45132ff4f72b8b74aed9edbe36f1fad0d
Edward Greve <edward.greve@gmail.com>
2016-04-03 10:24:58 +0800

    Add new methods to prototype directly rather than extending model to pass Error class inheritance tests.

--- src/plugins/pagination.js
+++ src/plugins/pagination.js
@@ -27,208 +27,204 @@ import { remove as _remove } from 'lodash';
  */
 module.exports = function paginationPlugin (bookshelf) {
 
-    bookshelf.Model = bookshelf.Model.extend({
+    /**
+     * @method Model#orderBy
+     * @since 0.9.3
+     * @description
+     *
+     * Specifies the column to sort on and sort order.
+     *
+     * The order parameter is optional, and defaults to 'ASC'. You may
+     * also specify 'DESC' order by prepending a hyphen to the sort column
+     * name. `orderBy("date", 'DESC')` is the same as `orderBy("-date")`.
+     *
+     * Unless specified using dot notation (i.e., "table.column"), the default
+     * table will be the table name of the model `orderBy` was called on.
+     *
+     * @example
+     *
+     * Cars.forge().orderBy('color', 'ASC').fetchAll()
+     *    .then(function (rows) { // ...
+     *
+     * @param sort {string}
+     *   Column to sort on
+     * @param order {string}
+     *   Ascending ('ASC') or descending ('DESC') order
+     */
+    bookshelf.Model.prototype.orderBy = function (sort, order) {
+        const tableName = this.constructor.prototype.tableName;
+        const idAttribute = this.constructor.prototype.idAttribute ?
+            this.constructor.prototype.idAttribute : 'id';
 
-        /**
-         * @method Model#orderBy
-         * @since 0.9.3
-         * @description
-         *
-         * Specifies the column to sort on and sort order.
-         *
-         * The order parameter is optional, and defaults to 'ASC'. You may
-         * also specify 'DESC' order by prepending a hyphen to the sort column
-         * name. `orderBy("date", 'DESC')` is the same as `orderBy("-date")`.
-         *
-         * Unless specified using dot notation (i.e., "table.column"), the default
-         * table will be the table name of the model `orderBy` was called on.
-         *
-         * @example
-         *
-         * Cars.forge().orderBy('color', 'ASC').fetchAll()
-         *    .then(function (rows) { // ...
-         *
-         * @param sort {string}
-         *   Column to sort on
-         * @param order {string}
-         *   Ascending ('ASC') or descending ('DESC') order
-         */
-        orderBy (sort, order) {
-            const tableName = this.constructor.prototype.tableName;
-            const idAttribute = this.constructor.prototype.idAttribute ?
-                this.constructor.prototype.idAttribute : 'id';
+        let _sort;
 
-            let _sort;
-
-            if (sort && sort.startsWith('-')) {
-                _sort = sort.slice(1);
-            } else if (sort) {
-                _sort = sort;
-            } else {
-                _sort = idAttribute;
-            }
-
-            const _order = order || (
-                (sort && sort.startsWith('-')) ? 'DESC' : 'ASC'
-            );
-
-            if (_sort.indexOf('.') === -1) {
-                _sort = `${tableName}.${_sort}`;
-            }
-
-            return this.query(qb => {
-                qb.orderBy(_sort, _order);
-            });
-        },
-
-        /**
-         * @method Model#fetchPage
-         * @since 0.9.3
-         * @description
-         *
-         * Similar to {@link Model#fetchAll}, but fetches a single page of results
-         * as specified by the limit (page size) and offset or page number.
-         *
-         * Any options that may be passed to {@link Model#fetchAll} may also be passed
-         * in the options to this method.
-         *
-         * To perform pagination, include a `limit` and _either_ `offset` or `page`.
-         *
-         * The defaults are page 1 (offset 0) and limit 10 when no parameters or invalid
-         * parameters are passed.
-         *
-         * Below is an example showing the user of a JOIN query with sort/ordering,
-         * pagination, and related models.
-         *
-         * @example
-         *
-         * Car
-         * .query(function (qb) {
-         *    qb.innerJoin('manufacturers', 'cars.manufacturer_id', 'manufacturers.id');
-         *    qb.groupBy('cars.id');
-         *    qb.where('manufacturers.country', '=', 'Sweden');
-         * })
-         * .orderBy('-productionYear') // Same as .orderBy('cars.productionYear', 'DESC')
-         * .fetchPage({
-         *    limit: 15, // Defaults to 10 if not specified
-         *    page: 3, // Defaults to 1 if not specified; same as {offset: 30} with limit of 15.
-         *    withRelated: ['engine'] // Passed to Model#fetchAll
-         * })
-         * .then(function (results) {
-         *    console.log(results); // Paginated results object with metadata example below
-         * })
-         *
-         * // Pagination results:
-         *
-         * {
-         *    models: [<Car>], // Regular bookshelf Collection
-         *    // other standard Collection attributes
-         *    ...
-         *    pagination: {
-         *        rowCount: 15, // Would be less than 15 on the last page of results
-         *        total: 53, // Total number of rows found for the query before pagination
-         *        limit: 15, // The requested number of rows per page, same as rowCount except final page
-         *        page: 3, // The requested page number
-         *        offset: 30 // The requested offset, calculated from the page/limit if not provided
-         *    }
-         * }
-         *
-         * @param options {object}
-         *    The pagination options, plus any additional options that will be passed to
-         *    {@link Model#fetchAll}
-         * @returns {Promise<Model|null>}
-         */
-        fetchPage (options = {}) {
-            const {limit, page, offset, ...fetchOptions} = options;
-
-            let _limit = parseInt(limit);
-            let _page = parseInt(page);
-            let _offset = parseInt(offset);
-
-            if (Number.isNaN(_limit) || !Number.isInteger(_limit) || _limit < 0) {
-                _limit = 10;
-            }
-
-            if (page && Number.isInteger(_page) && _page > 0) {
-                // Request by page number, calculate offset
-                _offset = _limit * (_page - 1);
-            } else if (offset && Number.isInteger(_offset) && _offset >= 0) {
-                // Request by offset, calculate page
-                _page = Math.floor(_offset / _limit) + 1;
-            } else {
-                // Defaults for erroneous or not defined page/offset
-                _page = 1;
-                _offset = 0;
-            }
-
-            const tableName = this.constructor.prototype.tableName;
-            const idAttribute = this.constructor.prototype.idAttribute ?
-                this.constructor.prototype.idAttribute : 'id';
-
-            const paginate = () => {
-                // const pageQuery = clone(this.query());
-                const pager = this.constructor.forge();
-
-                return pager
-
-                    .query(qb => {
-                        Object.assign(qb, this.query().clone());
-                        qb.limit.apply(qb, [_limit]);
-                        qb.offset.apply(qb, [_offset]);
-                        return null;
-                    })
-
-                    .fetchAll(fetchOptions);
-            };
-
-            const count = () => {
-                const notNeededQueries = [
-                    'orderByBasic',
-                    'orderByRaw',
-                    'groupByBasic',
-                    'groupByRaw'
-                ];
-                const counter = this.constructor.forge();
-
-                return counter
-
-                    .query(qb => {
-                        Object.assign(qb, this.query().clone());
-
-                        // Remove grouping and ordering. Ordering is unnecessary
-                        // for a count, and grouping returns the entire result set
-                        // What we want instead is to use `DISTINCT`
-                        qb.countDistinct.apply(qb, [`${tableName}.${idAttribute}`]);
-                        _remove(qb._statements, statement => notNeededQueries.indexOf(statement.type) > -1);
-                    })
-
-                    .fetchAll()
-
-                    .then(result => {
-                        const metadata = {page: _page, limit: _limit, offset: _offset};
-
-                        if (result && result.length == 1) {
-                            // We shouldn't have to do this, instead it should be
-                            // result.models[0].get('count')
-                            // but SQLite uses a really strange key name.
-                            const count = result.models[0];
-                            const keys = Object.keys(count.attributes);
-                            if (keys.length === 1) {
-                                const key = Object.keys(count.attributes)[0];
-                                metadata.total = count.attributes[key];
-                            }
-                        }
-
-                        return metadata;
-                    });
-            };
-
-            return Promise.join(paginate(), count())
-                .then(([rows, metadata]) => {
-                    const pageData = Object.assign(metadata, {rowCount: rows.length});
-                    return Object.assign(rows, {pagination: pageData});
-                });
+        if (sort && sort.startsWith('-')) {
+            _sort = sort.slice(1);
+        } else if (sort) {
+            _sort = sort;
+        } else {
+            _sort = idAttribute;
         }
 
-    });
+        const _order = order || (
+            (sort && sort.startsWith('-')) ? 'DESC' : 'ASC'
+        );
+
+        if (_sort.indexOf('.') === -1) {
+            _sort = `${tableName}.${_sort}`;
+        }
+
+        return this.query(qb => {
+            qb.orderBy(_sort, _order);
+        });
+    }
+
+    /**
+     * @method Model#fetchPage
+     * @since 0.9.3
+     * @description
+     *
+     * Similar to {@link Model#fetchAll}, but fetches a single page of results
+     * as specified by the limit (page size) and offset or page number.
+     *
+     * Any options that may be passed to {@link Model#fetchAll} may also be passed
+     * in the options to this method.
+     *
+     * To perform pagination, include a `limit` and _either_ `offset` or `page`.
+     *
+     * The defaults are page 1 (offset 0) and limit 10 when no parameters or invalid
+     * parameters are passed.
+     *
+     * Below is an example showing the user of a JOIN query with sort/ordering,
+     * pagination, and related models.
+     *
+     * @example
+     *
+     * Car
+     * .query(function (qb) {
+     *    qb.innerJoin('manufacturers', 'cars.manufacturer_id', 'manufacturers.id');
+     *    qb.groupBy('cars.id');
+     *    qb.where('manufacturers.country', '=', 'Sweden');
+     * })
+     * .orderBy('-productionYear') // Same as .orderBy('cars.productionYear', 'DESC')
+     * .fetchPage({
+     *    limit: 15, // Defaults to 10 if not specified
+     *    page: 3, // Defaults to 1 if not specified; same as {offset: 30} with limit of 15.
+     *    withRelated: ['engine'] // Passed to Model#fetchAll
+     * })
+     * .then(function (results) {
+     *    console.log(results); // Paginated results object with metadata example below
+     * })
+     *
+     * // Pagination results:
+     *
+     * {
+     *    models: [<Car>], // Regular bookshelf Collection
+     *    // other standard Collection attributes
+     *    ...
+     *    pagination: {
+     *        rowCount: 15, // Would be less than 15 on the last page of results
+     *        total: 53, // Total number of rows found for the query before pagination
+     *        limit: 15, // The requested number of rows per page, same as rowCount except final page
+     *        page: 3, // The requested page number
+     *        offset: 30 // The requested offset, calculated from the page/limit if not provided
+     *    }
+     * }
+     *
+     * @param options {object}
+     *    The pagination options, plus any additional options that will be passed to
+     *    {@link Model#fetchAll}
+     * @returns {Promise<Model|null>}
+     */
+    bookshelf.Model.prototype.fetchPage = function (options = {}) {
+        const {limit, page, offset, ...fetchOptions} = options;
+
+        let _limit = parseInt(limit);
+        let _page = parseInt(page);
+        let _offset = parseInt(offset);
+
+        if (Number.isNaN(_limit) || !Number.isInteger(_limit) || _limit < 0) {
+            _limit = 10;
+        }
+
+        if (page && Number.isInteger(_page) && _page > 0) {
+            // Request by page number, calculate offset
+            _offset = _limit * (_page - 1);
+        } else if (offset && Number.isInteger(_offset) && _offset >= 0) {
+            // Request by offset, calculate page
+            _page = Math.floor(_offset / _limit) + 1;
+        } else {
+            // Defaults for erroneous or not defined page/offset
+            _page = 1;
+            _offset = 0;
+        }
+
+        const tableName = this.constructor.prototype.tableName;
+        const idAttribute = this.constructor.prototype.idAttribute ?
+            this.constructor.prototype.idAttribute : 'id';
+
+        const paginate = () => {
+            // const pageQuery = clone(this.query());
+            const pager = this.constructor.forge();
+
+            return pager
+
+                .query(qb => {
+                    Object.assign(qb, this.query().clone());
+                    qb.limit.apply(qb, [_limit]);
+                    qb.offset.apply(qb, [_offset]);
+                    return null;
+                })
+
+                .fetchAll(fetchOptions);
+        };
+
+        const count = () => {
+            const notNeededQueries = [
+                'orderByBasic',
+                'orderByRaw',
+                'groupByBasic',
+                'groupByRaw'
+            ];
+            const counter = this.constructor.forge();
+
+            return counter
+
+                .query(qb => {
+                    Object.assign(qb, this.query().clone());
+
+                    // Remove grouping and ordering. Ordering is unnecessary
+                    // for a count, and grouping returns the entire result set
+                    // What we want instead is to use `DISTINCT`
+                    qb.countDistinct.apply(qb, [`${tableName}.${idAttribute}`]);
+                    _remove(qb._statements, statement => notNeededQueries.indexOf(statement.type) > -1);
+                })
+
+                .fetchAll()
+
+                .then(result => {
+                    const metadata = {page: _page, limit: _limit, offset: _offset};
+
+                    if (result && result.length == 1) {
+                        // We shouldn't have to do this, instead it should be
+                        // result.models[0].get('count')
+                        // but SQLite uses a really strange key name.
+                        const count = result.models[0];
+                        const keys = Object.keys(count.attributes);
+                        if (keys.length === 1) {
+                            const key = Object.keys(count.attributes)[0];
+                            metadata.total = count.attributes[key];
+                        }
+                    }
+
+                    return metadata;
+                });
+        };
+
+        return Promise.join(paginate(), count())
+            .then(([rows, metadata]) => {
+                const pageData = Object.assign(metadata, {rowCount: rows.length});
+                return Object.assign(rows, {pagination: pageData});
+            });
+    }
 }
