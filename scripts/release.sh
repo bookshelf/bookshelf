d08070e100d3f4e04cbfa3afc9ab26cd2721465c
Kaarel Raspel <kaarel.raspel@gmail.com>
2016-06-21 12:27:06 +0300

    Update version already in `master` branch. [ci skip]

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,13 +4,6 @@
 git checkout master
 git pull
 
-# Switch to `release` branch (create it if it doesn't exist) and update it to latest
-git checkout -B release
-git pull
-
-# Get current state from `master` branch
-git merge -Xtheirs master
-
 get_property() {
   echo $(node -p "p=require('./${1}').${2};")
 }
@@ -36,6 +29,17 @@ if ! [[ $next_version =~ ^[0-9]\.[0-9]+\.[0-9](-.+)? ]]; then
   exit 1
 fi
 
+# Update version in package.json before updating `release` branch.
+update_version 'package.json' $next_version
+git commit -am "Update version to $next_version."
+
+# Switch to `release` branch (create it if it doesn't exist) and update it to latest
+git checkout -B release
+git pull
+
+# Get current state from `master` branch
+git merge -Xtheirs master
+
 # Now clean up those force added files, we'll have to add another commit.
 # First remove all files in the repo (including those ignored files that we
 # force added).
@@ -54,10 +58,6 @@ npm run jsdoc
 # We must force add because these are usually ignored.
 git add --force --all index.html docs lib
 
-# Update version in package.json before running tests (tests will catch if the
-# version number is out of sync).
-update_version 'package.json' $next_version
-
 # Remove the postinstall script because we'll be including the built files in
 # this commit.
 delete_property 'package.json' 'scripts.postinstall'
@@ -67,15 +67,17 @@ npm test
 git commit -am "Release $next_version."
 git tag $next_version
 
-git push origin release
-git push origin release --tags
-
 echo "# Publishing docs"
 
 echo "$(git checkout -B gh-pages)"
 echo "$(git pull)"
 echo "$(git merge -Xtheirs release)"
+echo "$(git checkout master)"
+
+# All good. Push changes to remote.
+git push origin master
+git push origin release
+git push origin release --tags
 git push origin gh-pages
-echo "$(git checkout release)"
 
 npm publish
