2e36e02eb3d802800c754ce783b0f4d09660aa83
Kaarel Raspel <kaarel.raspel@gmail.com>
2016-06-21 12:14:16 +0300

    Update releasing script to use `release` branch for releases and version tags

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -e
 
+# Update master to the latest
 git checkout master
+git pull
+
+# Switch to `release` branch (create it if it doesn't exist) and update it to latest
+git checkout -B release
+git pull
+
+# Get current state from `master` branch
+git merge -Xtheirs master
 
 get_property() {
   echo $(node -p "p=require('./${1}').${2};")
@@ -11,11 +20,6 @@ delete_property() {
   echo "deleted '${2}' from './${1}'"
 }
 
-set_property() {
-  echo "$(node -p "p=require('./${1}');p.${2}=${3};JSON.stringify(p, null, 2)")" > $1
-  echo "set '${2}' to '${3}' in './${1}'"
-}
-
 update_version() {
   echo "$(set_property ${1} 'version' "'${2}'")"
   echo "Updated ${1} version to ${2}"
@@ -24,7 +28,6 @@ update_version() {
 # Remove the postinstall script, because we're going to build everything here.
 current_version="$(get_property 'package.json' 'version')"
 
-printf "Please ensure that the version number has been updated in bookshelf.js and src/bookshelf.js before continuing.\n"
 printf "Next version (current is $current_version)? "
 read next_version
 
@@ -33,14 +36,20 @@ if ! [[ $next_version =~ ^[0-9]\.[0-9]+\.[0-9](-.+)? ]]; then
   exit 1
 fi
 
-git add -u
+# Now clean up those force added files, we'll have to add another commit.
+# First remove all files in the repo (including those ignored files that we
+# force added).
+echo "$(git rm -r --cached .)"
+
+# Now add all files, this will exclude ignored files.
+echo "$(git add .)"
+
+# Delete all ignored files and folders
+echo "$(git clean -xdf)"
+
+# Build JS from ES6 and generate JSDoc
 npm run build
 npm run jsdoc
-
-# Remove the postinstall script because we'll be including the built files in
-# this commit.
-original_postinstall="$(get_property 'package.json' 'scripts.postinstall')"
-delete_property 'package.json' 'scripts.postinstall'
 
 # We must force add because these are usually ignored.
 git add --force --all index.html docs lib
@@ -49,38 +58,24 @@ git add --force --all index.html docs lib
 # version number is out of sync).
 update_version 'package.json' $next_version
 
+# Remove the postinstall script because we'll be including the built files in
+# this commit.
+delete_property 'package.json' 'scripts.postinstall'
+
 npm test
 
 git commit -am "Release $next_version."
 git tag $next_version
 
-git push origin master
-git push origin master --tags
+git push origin release
+git push origin release --tags
 
 echo "# Publishing docs"
 
 echo "$(git checkout -B gh-pages)"
 echo "$(git pull)"
-echo "$(git merge master)"
+echo "$(git merge -Xtheirs release)"
 git push origin gh-pages
-echo "$(git checkout master)"
+echo "$(git checkout release)"
 
 npm publish
-
-# Now clean up those force added files, we'll have to add another commit.
-# First remove all files in the repo (including those ignored files that we
-# force added.
-echo "$(git rm -r --cached .)"
-
-# Reset package.json.
-set_property 'package.json' 'scripts.postinstall' "'$original_postinstall'"
-
-# Now add all files, this will exclude ignored files.
-echo "$(git add .)"
-
-# Sleeping here space out commits (graph looked weird in SourceTree).
-sleep 1
-
-echo "$(git commit -m "Remove lib and docs after $next_version release.")"
-
-git push origin master
