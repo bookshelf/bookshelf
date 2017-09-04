3bfa79a715c0ad8e49baccb0294c8b2b79dcb10f
Olli Jaakkola <olli.jaakkola@leonidasoy.fi>
2017-09-04 15:33:46 +0300

    Use isNil instead of null check so that undefined values are also caught

--- src/relation.js
+++ src/relation.js
@@ -401,7 +401,7 @@ export default RelationBase.extend({
     // Loop over the `parentModels` and attach the grouped sub-models,
     // keeping the `relatedData` on the new related instance.
     _.each(parentModels, (model) => {
-      let groupedKey = null;
+      let groupedKey;
       if (!this.isInverse()) {
         groupedKey = model.get(this.parentIdAttribute);
       } else {
@@ -411,7 +411,7 @@ export default RelationBase.extend({
         const formatted = model.format(_.clone(model.attributes));
         groupedKey = formatted[keyColumn];
       }
-      if (groupedKey !== null) {
+      if (!_.isNil(groupedKey)) {
         const relation = model.relations[relationName] = this.relatedInstance(grouped[groupedKey]);
         relation.relatedData = this;
         if (this.isJoined()) _.extend(relation, pivotHelpers);
