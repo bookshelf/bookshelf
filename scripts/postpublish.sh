e64aa358ef96c24f5c60441551a580dedca9fbcc
Ricardo Graça <ricardo@devius.net>
2018-01-08 19:17:03 +0000

    Release 0.12.1

--- scripts/postpublish.sh
+++ scripts/postpublish.sh
@@ -8,7 +8,7 @@ echo "Getting version number from package.json"
 version="$(get_property 'package.json' 'version')"
 
 echo "(Re)Creating bookshelf-source remote"
-git remote remove bookshelf-source
+git remote remove bookshelf-source || true
 git remote add bookshelf-source git@github.com:bookshelf/bookshelf.git
 
 echo "Committing new release version w/ any outstanding changes"
