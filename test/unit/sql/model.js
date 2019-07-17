9c757cd5d5cccc967586fddc1237eb941eead29a
Ricardo Graça <ricardo@devius.net>
2019-07-17 22:16:45 +0100

    Move toJSON() model tests to unit directory
    - These are not integration tests since they are only testing the 
    internals of the .toJSON() method.

--- test/unit/sql/model.js
+++ test/unit/sql/model.js
@@ -1,12 +1,12 @@
 var Promise = global.testPromise;
-var equal = require('assert').equal;
-var notStrictEqual = require('assert').notStrictEqual;
+var {deepEqual, equal, notStrictEqual} = require('assert');
 var _ = require('lodash');
 var path = require('path');
 var basePath = process.cwd();
 
 module.exports = function() {
-  var Model = require(path.resolve(basePath + '/lib/model'));
+  const Model = require(path.resolve(basePath, 'lib/model'));
+  const Collection = require(path.resolve(basePath, 'lib/collection'));
 
   describe('Model', function() {
     describe('#save()', function() {
@@ -63,6 +63,98 @@ module.exports = function() {
 
         expect(model.get('created_at')).to.not.exist;
         expect(model.get('updated_at')).to.not.exist;
+      });
+    });
+
+    describe('#toJSON()', function() {
+      let ModelCollection;
+      let testModel;
+
+      before(() => {
+        ModelCollection = Collection.extend({model: Model});
+      });
+
+      beforeEach(() => {
+        testModel = new Model({id: 1, name: 'Test'});
+      });
+
+      it('includes the idAttribute in the hash', function() {
+        const DifferentModel = Model.extend({idAttribute: '_id'});
+        const testModel = new DifferentModel({_id: 1, name: 'Joe'});
+
+        deepEqual(testModel.toJSON(), {_id: 1, name: 'Joe'});
+      });
+
+      it('includes the relations loaded on the model', function() {
+        testModel.relations = {
+          someList: new ModelCollection([{id: 1}, {id: 2}])
+        };
+        var json = testModel.toJSON();
+
+        deepEqual(Object.keys(json), ['id', 'name', 'someList']);
+        equal(json.someList.length, 2);
+      });
+
+      it("doesn't include the relations loaded on the model if {shallow: true} is passed", function() {
+        testModel.relations = {
+          someList: new ModelCollection([{id: 1}, {id: 2}])
+        };
+        var shallow = testModel.toJSON({shallow: true});
+
+        deepEqual(_.keys(shallow), ['id', 'name']);
+      });
+
+      it('does not omit new models from collections and relations when {omitNew: false} is passed', function() {
+        testModel.relations = {
+          someList: new ModelCollection([{id: 2}, {attr2: 'Test'}]),
+          someRel: new Model({id: 3}),
+          otherRel: new Model({attr3: 'Test'})
+        };
+        var coll = new ModelCollection([testModel, new Model({attr5: 'Test'}), new Model({id: 4, attr4: 'Test'})]);
+        var json = coll.toJSON({omitNew: false});
+
+        equal(json.length, 3);
+        equal(json[0].someList.length, 2);
+        deepEqual(_.keys(json[0]), ['id', 'name', 'someList', 'someRel', 'otherRel']);
+        deepEqual(_.keys(json[1]), ['attr5']);
+        deepEqual(_.keys(json[2]), ['id', 'attr4']);
+      });
+
+      it('does not omit new models from collections and relations when omitNew is not specified', function() {
+        testModel.relations = {
+          someList: new ModelCollection([{id: 2}, {attr2: 'Test'}]),
+          someRel: new Model({id: 3}),
+          otherRel: new Model({attr3: 'Test'})
+        };
+        var coll = new ModelCollection([testModel, new Model({attr5: 'Test'}), new Model({id: 4, attr4: 'Test'})]);
+        var json = coll.toJSON();
+
+        equal(json.length, 3);
+        equal(json[0].someList.length, 2);
+        deepEqual(_.keys(json[0]), ['id', 'name', 'someList', 'someRel', 'otherRel']);
+        deepEqual(_.keys(json[1]), ['attr5']);
+        deepEqual(_.keys(json[2]), ['id', 'attr4']);
+      });
+
+      it('omits new models from collections and relations when {omitNew: true} is passed', function() {
+        testModel.relations = {
+          someList: new ModelCollection([{id: 2}, {attr2: 'Test'}]),
+          someRel: new Model({id: 3}),
+          otherRel: new Model({attr3: 'Test'})
+        };
+        var coll = new ModelCollection([testModel, new Model({attr5: 'Test'}), new Model({id: 4, attr4: 'Test'})]);
+        var omitNew = coll.toJSON({omitNew: true});
+
+        equal(omitNew.length, 2);
+        deepEqual(_.keys(omitNew[0]), ['id', 'name', 'someList', 'someRel']);
+        deepEqual(_.keys(omitNew[1]), ['id', 'attr4']);
+        equal(omitNew[0].someList.length, 1);
+      });
+
+      it('returns null for a new model when {omitNew: true} is passed', function() {
+        var testModel = new Model({attr1: 'Test'});
+        var omitNew = testModel.toJSON({omitNew: true});
+        deepEqual(omitNew, null);
       });
     });
 
