d240109f84242588b33280c4066707786de2aab3
Antti Lehto <anajavi@users.noreply.github.com>
2019-10-04 22:47:25 +0300

    Add tests for JSON.stringify Model objects

--- test/unit/model.js
+++ test/unit/model.js
@@ -223,6 +223,21 @@ module.exports = function() {
 
         deepEqual(json, {id: 1, firstName: 'Joe', lastName: 'Shmoe', address: '123 Main St.'});
       });
+      describe('with JSON.stringify', function() {
+        it('serializes correctly when placed as object property', function() {
+          testModel.visible = ['firstName'];
+          var obj = {
+            model: testModel
+          };
+          deepEqual(JSON.stringify(obj), '{"model":{"firstName":"Joe"}}');
+        });
+
+        it('serializes correctly when placed in an array', function() {
+          testModel.visible = ['firstName'];
+          var arr = [testModel];
+          deepEqual(JSON.stringify(arr), '[{"firstName":"Joe"}]');
+        });
+      });
     });
 
     describe('#hasChanged()', function() {
